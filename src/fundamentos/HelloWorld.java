1245173adfab3669b28ec0d4cdc9cb298b19d021
Marcklen <marcklen@icloud.com>
2022-10-04 19:43:48 -0300

    concatenando string mjv

--- src/fundamentos/HelloWorld.java
+++ src/fundamentos/HelloWorld.java
@@ -3,7 +3,12 @@ package fundamentos;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Ola Mundo!");
+
+//		System.out.println("Ola Mundo!");
+
+		String nome = "Marcklen";
+		String profissao = "Desenvolvedor BackEnd Junior";
+		String futuro = nome.concat(" " + profissao);
+		System.out.println(futuro);
 	}
 }
