c81efe4b2204587d1e1343acc3f34199b06dce89
Marcklen <marcklen@icloud.com>
2022-10-05 18:59:42 -0300

    adicionando metodos a c.corrente

--- src/exercicio_04_10_22/ContaCorrente.java
+++ src/exercicio_04_10_22/ContaCorrente.java
@@ -1,9 +1,54 @@
 package exercicio_04_10_22;
 
+import java.util.Date;
+import java.util.List;
+
 public class ContaCorrente {
 
-	private int agencia;
-	private int numero;
-	private double saldo;
+	int numeroConta;
+	int numeroAgencia;
+	String nomeCliente;
+	Date dataNascimento;
+	double saldoConta;
 	
+	//depositarValores
+	void depositar(double valor) {
+		if(valor <= 0) {
+			System.out.println("Erro, não há como depositar valores menores que R$ 0,00 reais");
+		}else {
+			saldoConta += valor;
+		}
+	}
+	//sacarValores();
+	void sacar(double valor) {
+		if(valor > saldoConta) {
+			System.out.println("Saldo insuficiente, consulte o seu extrato para verificar qual valor você pode sacar!");
+		}else {
+			saldoConta -= valor;
+		}
+	}
+	//transferirValores();
+	void transferir(double valor, ContaCorrente conta) {
+		conta = new ContaCorrente();
+		
+	}
+	//cancelarContaComJustificativa
+	boolean cancelar(ContaCorrente conta, String justificativa) {
+		return false;
+	}
+	//consultarExtrato
+	List<?> consultarExtrato(Date inicio, Date fim) {
+		if(fim.after(inicio)) {
+			System.out.println("ERRO");
+		}
+		return List.of(inicio, fim);
+	}
+	//consultarSaldoAtual
+	double consultarSaldo() {
+		System.out.println("Agencia:       " + numeroAgencia);
+		System.out.println("ContaCorrente: " + numeroConta);
+		System.out.println("Cliente:       " + nomeCliente);
+		System.out.print("Saldo Atual:     ");
+		return saldoConta;
+	}
 }
